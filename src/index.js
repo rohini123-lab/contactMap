70bfec2a7407efccbd80bb6a8e0944ca9b3706b6
Rohini <118761078+rohini123-lab@users.noreply.github.com>
2023-08-27 20:54:19 +1000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import store from './Store/Store';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/contactMap">
     <Provider store={store}>
              <App />
             </Provider>
